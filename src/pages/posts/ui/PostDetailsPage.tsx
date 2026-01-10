947b03feb0cc387b7cb16ca894e36deacd097a4f
Никита <stovolosovn@gmail.com>
2026-01-10 19:36:11 +0300

    Used a hook on the PostDitailsPage

--- src/pages/posts/ui/PostDetailsPage.tsx
+++ src/pages/posts/ui/PostDetailsPage.tsx
@@ -1,5 +1,26 @@
+import { useParams } from "react-router-dom";
+
+import type { PostParams } from "@/App/providers/router/ui/AppRouter";
+import PostCard from "@/entities/post/ui/PostCard";
+import { usePost } from "@/features/PostList/model/hooks/usePosts";
+
+import styles from "./PostDetailsPage.module.css";
+
 const PostDetailsPage = () => {
-  return <h1>Детали поста</h1>;
+  const { id } = useParams<PostParams>();
+
+  const { post, isLoading, error } = usePost(id || "");
+
+  if (isLoading) return <p>Загрузка поста...</p>;
+  if (error) return <p>Ошибка при загрузке: {error}</p>;
+  if (!post) return <p>Пост не найден.</p>;
+
+  return (
+    <div className={styles.page}>
+      <h1>Детали поста</h1>
+      <PostCard post={post} />
+    </div>
+  );
 };
 
 export default PostDetailsPage;
