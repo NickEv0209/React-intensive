60637e49756c675d76dfd1d98a10a7b26ef7841a
NickEv <stovolosovn@gmail.com>
2026-01-22 03:08:21 +0300

    Updated the UserAlbumsPage and UserTodoPage

--- src/pages/users/ui/UserAlbumsPage.tsx
+++ src/pages/users/ui/UserAlbumsPage.tsx
@@ -1,5 +1,13 @@
+import { useGetAlbumsQuery } from "@/entities/album/api/albumsApi";
+
 const UserAlbumsPage = () => {
-  return <h1>UserAlbumsPage</h1>;
+  const { data: albums, isLoading } = useGetAlbumsQuery(1);
+
+  if (isLoading) return <h3>Loading...</h3>;
+
+  return albums?.map((album) => {
+    return <h3 key={album.id}>{album.title}</h3>;
+  });
 };
 
 export default UserAlbumsPage;
