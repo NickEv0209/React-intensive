8e92eccb19e0159bf725c01b9e0cd7cd50f6251d
NickEv <stovolosovn@gmail.com>
2026-01-22 03:06:54 +0300

    Updated the postPage

--- src/pages/posts/ui/PostsPage.tsx
+++ src/pages/posts/ui/PostsPage.tsx
@@ -1,32 +1,22 @@
-import { useEffect, useMemo, useState } from "react";
-import axios from "axios";
+import { useMemo, useState } from "react";
 
 import { PostListWithLoading } from "@/widgets/PostList/PostList";
-import type { PostType } from "@/entities/post/model/types";
+
 import {
   filterByLength,
   PostLengthFilter,
   type PostLengthVariant,
 } from "@/features/PostLengthFilter";
+import { useGetPostsByUserIdQuery } from "@/entities/post/api/postsApi";
+import type { PostType } from "@/entities/post/model/types";
 
 const PostPage = () => {
-  const [posts, setPosts] = useState<PostType[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
   const [filterVariant, setFilterVariant] = useState<PostLengthVariant>("all");
 
-  useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get("/api/posts")
-      .then((res) => {
-        setPosts(res.data);
-        setIsLoading(false);
-      })
-      .catch((err) => console.error("Ошибка запроса: " + err));
-  }, []);
+  const { data: posts, isLoading } = useGetPostsByUserIdQuery(1);
 
   const filteredPosts = useMemo(() => {
-    return filterByLength(posts, filterVariant);
+    return filterByLength(posts as PostType[], filterVariant);
   }, [posts, filterVariant]);
 
   return (
